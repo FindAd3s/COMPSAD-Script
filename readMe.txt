1effdf4072ee1e4d164ba976f3a2ea3cc8ffad48
FindAd3s <36985360+FindAd3s@users.noreply.github.com>
2022-06-07 19:03:31 +0800

    First Commit

---
+++ readMe.txt
@@ -0,0 +1,18 @@
+Setup Instructions
+
+
+1. Ensure you have python and pip, and XAMPP MySQL installed on your device
+
+2. To install the necessary libraries needed for the code, run "pip install -r requirements.txt" (Python 2), or "pip3 install -r requirements.txt" (Python 3) [Any of these is fine]
+
+3. Start Apache and MySQL on XAMPP
+
+4. Create the necessary database titled 'fies" on MySQL
+
+5. Running the script
+    a. First run of the script
+        - Run 'initializeTable.py' to create the necessary table on the MySQL database
+    
+    b. Every other run of the script
+        - Run 'uploadToMySQL.py' to publish the edits done onto the Excel file attached.
+        - Before running, save the excel file to ensure that the data is successfully uploaded.
---
+++ uploadToMySQL.py
@@ -0,0 +1,19 @@
+from sqlalchemy import create_engine
+import pandas as pd
+
+print ("hello") #Hello
+engine = create_engine("mysql+pymysql://root@localhost/fies") #Connection address to the SQL server
+conn = engine.connect() #Actual connection to the SQL Server
+
+referencedata = pd.read_sql_table("fies_2018_2012", con = engine) #Reads data from SQL
+
+excelData = pd.read_excel('2018-2012_FIES_excel_data.xlsx', sheet_name='2018 FIES data') #Reads data on the excel file
+
+# Uncomment print statement below to print excel data for reference
+# print(excelData) 
+
+concatenateData = pd.concat([excelData, referencedata], axis = 0) #Merges Excel data to SQL data. Excel Data is always on top 
+concatenateData = concatenateData.drop_duplicates(subset=['Region'], keep = 'first', ignore_index= True) #Drops duplicates based on Region. Newest data is kept
+
+frame = concatenateData.to_sql("fies_2018_2012", conn, if_exists = 'replace', index = False) #Uploads excel data to the SQL Server
+print("data uploaded to sql server")
